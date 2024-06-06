9a924fdc8887bbd7cfb394c5ca43955e2c6952dd
sarahrubia <sarahrubia22@gmail.com>
2024-06-06 16:58:26 -0300

    fix: add closing to scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -17,6 +17,8 @@ public class ContaTerminal {
     System.out.println("Por favor, digite o seu saldo.");
     Double Saldo = scanner.nextDouble();
 
+    scanner.close();
+
     System.out.println(String.format(
         "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.",
         NomeCliente, Agencia, Numero, Saldo));
